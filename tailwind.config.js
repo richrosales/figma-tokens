658143a2a4ac7c9a1a5f3299df33cafd64e6b6f5
Rich Rosales <rich.rosales@boardriders.com>
2021-11-01 17:53:28 -0700

    updating tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 
 const { filterTokensByType } = require("./fns");
-const tokens = require("./output/light.json")
+// const tokens = require("./output/light.json")
+const tokens = require("./output/sds.json")
 
 const colors = filterTokensByType('color', tokens)
 
